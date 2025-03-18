05d1789dfbd119fe4d135d74596d8fa0fb7a6824
MatheusNevs <matheusnf62@gmail.com>
2025-03-18 13:26:07 -0300

    fix: auth layout fixed

--- app/src/app/(auth)/createMonitor/layout.tsx
+++ app/src/app/(auth)/createMonitor/layout.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { permanentRedirect } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { toast } from "sonner";
 
 export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const session = useSession();
-  if (!session.data) {
-    permanentRedirect("/");
-  }
+
+  const initialized = useRef(false);
+  useEffect(() => {
+    if (initialized.current) return;
+    initialized.current = true;
+
+    if (!session.data) {
+      toast.error("Para acessar deve estar logado");
+      permanentRedirect("/");
+    }
+  }, [session.data]);
   return <>{children}</>;
 }
